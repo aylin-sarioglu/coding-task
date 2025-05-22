64340ab3d62cbf333bf51f8d9f181da71c0548cf
Aylin Sarioglu <aylin.sarioglu@outlook.at>
2025-05-22 11:40:37 +0200

    feat: add core ui elements for AnalysisResultComponent

--- frontend/text-analyzer-app/src/components/analysis-result/analysis-result.component.ts
+++ frontend/text-analyzer-app/src/components/analysis-result/analysis-result.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-analysis-result',
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './analysis-result.component.html',
-  styleUrl: './analysis-result.component.css'
+  styleUrl: './analysis-result.component.css',
 })
-export class AnalysisResultComponent {
-
-}
+export class AnalysisResultComponent {}
