52d982167fe2f822bee5409d3718ccd64d1feac4
Aylin Sarioglu <aylin.sarioglu@outlook.at>
2025-05-22 21:56:20 +0200

    style: introduce cards for result presentation, small overall UI improvements

--- frontend/text-analyzer-app/src/components/analyzer-result/analyzer-result.component.ts
+++ frontend/text-analyzer-app/src/components/analyzer-result/analyzer-result.component.ts
@@ -3,10 +3,11 @@ import { MatListModule } from '@angular/material/list';
 import { TextAnalyzerResponse } from '../../services/text-analyzer';
 import { CommonModule } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-analyzer-result',
-  imports: [MatListModule, CommonModule, MatChipsModule],
+  imports: [MatListModule, CommonModule, MatChipsModule, MatCardModule],
   templateUrl: './analyzer-result.component.html',
   styleUrl: './analyzer-result.component.css',
 })
